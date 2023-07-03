9fdb56f8f17d348ce5db298c79007b3e6fcd9598
jay <jay.mm.message@gmail.com>
2023-07-03 14:33:54 +0800

    Access system's environment variables

---
+++ 01_TheBasics/t_env.py
@@ -0,0 +1,9 @@
+import os
+
+print("system's environment: ", os.environ)
+
+print("\n")
+print("system's env('home): ", os.getenv('HOME'))
+
+print("\n")
+print("system's env('language): ", os.getenv('LANG'))
