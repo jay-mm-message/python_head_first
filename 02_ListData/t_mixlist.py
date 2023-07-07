0441f270b32475c992cf84d1a799a5a3a5529c91
jay <jay.mm.message@gmail.com>
2023-07-07 20:03:05 +0800

    Create a mixed list

---
+++ 02_ListData/t_mixlist.py
@@ -0,0 +1,11 @@
+
+list_data_1 = [1, 2, 3]
+list_data_2 = ['a', 'b', 'c']
+list_data_3 = [ 'One', 'Two', 'Three']
+
+list_for_all = []
+list_for_all.append(list_data_1)
+list_for_all.append(list_data_2)
+list_for_all.append(list_data_3)
+
+print(list_for_all)
