2aa8fbfa29203ec14dff37c0a5cd301f692a32d8
jay <jay.mm.message@gmail.com>
2023-07-07 20:04:48 +0800

    Remove garbage

--- 02_ListData/t_mixlist.py
+++ 02_ListData/t_mixlist.py
@@ -1,7 +1,7 @@
 
 list_data_1 = [1, 2, 3]
 list_data_2 = ['a', 'b', 'c']
-list_data_3 = [ 'One', 'Two', 'Three']
+list_data_3 = ['One', 'Two', 'Three']
 
 list_for_all = []
 list_for_all.append(list_data_1)
